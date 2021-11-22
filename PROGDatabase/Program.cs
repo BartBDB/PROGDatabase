d8e4124c175f22182afc154b01d66d653060d5d1
Bart de Boer <bartdb2001@gmail.com>
2021-11-22 14:46:48 +0100

    first commit

---
+++ PROGDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROGDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
