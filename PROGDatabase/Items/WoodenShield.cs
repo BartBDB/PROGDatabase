417ac4a738b648dc3066df407aad6a8586ef0a22
Bart de Boer <bartdb2001@gmail.com>
2022-01-17 14:06:49 +0100

    update

---
+++ PROGDatabase/Items/WoodenShield.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGDatabase
+{
+    class WoodenShield : ItemStats
+    {
+        private int defense = 2;
+        public WoodenShield(string name, string description) : base(name, description)
+        {
+        }
+    }
+}
--- PROGDatabase/Program.cs
+++ PROGDatabase/Program.cs
@@ -12,6 +12,9 @@ namespace PROGDatabase
             ItemStats woodenSword = new WoodenSword("Wooden Sword",
                 "A very basic wooden sword. Does the job either as a weapon or as firewood. It may be wood, but sitting on top of it is not recommended.");
 
+            ItemStats woodenShield = new WoodenShield("Wooden Shield",
+                "A very basic wooden shield. Won't do much for you, apart from being a great seat for when you are tired.");
+            
             database.AddData(new Item("Sword"));
             database.AddData(new Item("Shield"));
 
